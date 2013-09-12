0d0bcea11eba69ee4407f70b2ce7bc7ba2022fa0
Ilter Cengiz <me@iltercengiz.info>
2013-09-12 15:18:56 +0300

    Removed target Watchtower, and created a new one. Pods integration. New setup of project and workspace.

---
+++ WatchTower/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WatchTower
+//
+//  Created by Ilter Cengiz on 12/09/2013.
+//  Copyright (c) 2013 Alexander Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WatchTower/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WatchTower
+//
+//  Created by Ilter Cengiz on 12/09/2013.
+//  Copyright (c) 2013 Alexander Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
